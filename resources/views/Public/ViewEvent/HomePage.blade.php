6338045014df57be113c6060a50451e7fbdefa42
LUCALOFFRE <lucaloft@libero.it>
2019-10-24 14:44:52 +0200

    homepage

---
+++ resources/views/Public/ViewEvent/HomePage.blade.php
@@ -0,0 +1,9 @@
+@extends('Public.ViewEvent.Layouts.master')
+
+@section('title')
+Homepage
+@endsection
+@section('content')
+  @include('Public.ViewEvent.Partials.head')
+    @include('Public.ViewEvent.Partials.homePageSection')
+@endsection('content')
