edf88f1ecb7f03d5d1681a668b1336068ba5695b
Uddin <JalalUddin.Ahammad@nttdata.com>
2019-10-24 09:32:03 +0200

    aggiunto meta-data csrf token, senza non funziona la post per laravel

--- resources/views/Public/ViewEvent/Layouts/master.blade.php
+++ resources/views/Public/ViewEvent/Layouts/master.blade.php
@@ -20,7 +20,7 @@
   <!-- Mobile Specific Metas
   ================================================== -->
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <!-- Vendor CSS
   ============================================ -->
 
