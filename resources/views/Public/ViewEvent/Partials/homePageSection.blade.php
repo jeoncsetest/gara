bc6b8705cb678c61cec65605713062b4f9a2776d
Uddin <JalalUddin.Ahammad@nttdata.com>
2019-10-29 09:16:28 +0100

    signup pop-up

--- resources/views/Public/ViewEvent/Partials/homePageSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/homePageSection.blade.php
@@ -33,7 +33,7 @@
 
         <div class="info-box" data-bg="{{ asset('/bootstrap/4.3.1/images/480x338_bg4.jpg') }}">
 
-          <h3 class="box-title">Serate</h3>
+           <h3 class="box-title">Serate</h3>
 <!--        <p>Aenean auctor wisi et urna. Aliquam <br> erat volutpat. Duis ac turpis. Donec sit <br> amet eros. Lorem ipsum dolor sit amet.</p>
           <a href="#" class="btn btn-style-2">Visualizza</a>
 -->
@@ -53,11 +53,17 @@
         <p>Already have an account? <a href="#" class="link-text var2 popup-btn-login">Login Here</a></p>
         <a href="#" class="btn fb-btn btn-big">Sign Up With Facebook</a>
         <p class="content-element2">OR</p>
+       
+       <!--###############################################################################-->
+  
+<!--##################################################################################################-->
+
         <form class="content-element2">
 
-          <input type="text" placeholder="Enter Your Email Address">
-          <input type="text" placeholder="Password">
-          <a href="#" class="btn btn-style-3 btn-big">Join Free Now!</a>
+                  <a href="{{route('showSignupSimple', array('signupType'=>'TICKET'))}}" class="btn btn-primary btn-lg active" role="button" aria-pressed="true"><i class="fas fa-users"></i> Pubblico</a>
+                  <a href="{{route('showSignupSimple', array('signupType'=>'STUDENT'))}}" class="btn btn-primary btn-lg active" role="button" aria-pressed="true"><i class="fas fa-users"></i> Ballerino</a>
+                  <a href="{{route('showSignupSimple', array('signupType'=>'SCHOOL'))}}" class="btn btn-primary btn-lg active" role="button" aria-pressed="true"><i class="fas fa-users"></i> Scuola</a>
+                            
 
         </form>
 
@@ -77,11 +83,10 @@
         <p>Don't have an account yet?<a href="#" class="link-text var2 popup-btn-sign">JOIN FOR FREE</a></p>
         <a href="#" class="btn fb-btn btn-big"> Login With Facebook</a>
         <p class="content-element2">OR</p>
-        <form class="content-element1">
-
-          <input type="text" placeholder="Enter Your Email Address">
-          <input type="text" placeholder="Password">
-          <a href="#" class="btn btn-style-3 btn-big">Login</a>
+        <form  method="post" class="content-element1">
+          <input name="email" id="myemail" type="text" placeholder="Enter Your Email Address">
+          <input name="password" id="mypassword" type="password" placeholder="Password">
+          <button type="button" id="loginBtn" class="btn btn-style-3 btn-big">Login</button
 
           <div class="input-wrapper">
             <input type="checkbox" id="checkbox11" name="checkbox">
